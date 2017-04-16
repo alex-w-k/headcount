761c095fa13377fc96e608d0b7a4b96850888988
alex549us3 <alex549us3@gmail.com>
2017-04-16 12:54:20 -0600

    adding all hs grad data to hash in enrollment object now

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -2,11 +2,16 @@ require 'pry'
 require 'csv'
 
 class Enrollment
-  attr_reader :name, :year, :data_format, :data, :kindergarten_participation
+  attr_reader :name, :kindergarten_participation
+  attr_accessor :high_school_graduation_rates
 
   def initialize(args)
     @name = args[:name]
     @kindergarten_participation = args[:kindergarten_participation]
+    @high_school_graduation_rates = args[:high_school_graduation]
+    if @high_school_graduation_rates.nil?
+      @high_school_graduation_rates = Hash.new([])
+    end
   end
 
   def kindergarten_participation_by_year
--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -7,12 +7,14 @@ class EnrollmentRepository
 
   def load_data(args)
     data_set = args[:enrollment][:kindergarten]
-    contents = CSV.open(data_set, {headers: true, header_converters: :symbol})
-    @enrollments = collect_enrollments(contents)
+    @hs_data_set = args[:enrollment][:high_school_graduation]
+    kindergarten_contents = CSV.open(data_set, {headers: true, header_converters: :symbol})
+    @enrollments = collect_enrollments(kindergarten_contents)
     @test = @enrollments.uniq do |enrollment|
       enrollment.name
     end
     collate_years
+    add_high_school_data_to_enrollments
   end
 
   def collect_enrollments(contents)
@@ -24,6 +26,20 @@ class EnrollmentRepository
                       :kindergarten_participation => 
                       {row[:timeframe] => row[:data]}})
     end
+  end
+
+  def add_high_school_data_to_enrollments
+    high_school_grad_contents = CSV.open(@hs_data_set, {headers: true, header_converters: :symbol})
+    high_school_grad_contents.each do |row|
+      row[:name] = row[:location].upcase
+      row[:timeframe] = row[:timeframe].to_i
+      row[:data] = row[:data].to_f
+      index = @enrollments.find_index do |enrollment|
+            enrollment.name == row[:location].upcase
+          end
+      @enrollments[index].high_school_graduation_rates[row[:timeframe]] = row[:data]
+    end
+  @enrollments
   end
 
   def find_by_name(name)
@@ -39,10 +55,14 @@ class EnrollmentRepository
     @enrollments.each do |enrollment|
       @test.each do |en|
       if enrollment.name == en.name
-        en.kindergarten_participation[enrollment.kindergarten_participation.keys.first] = enrollment.kindergarten_participation.values.first
+        en.kindergarten_participation[enrollment.kindergarten_participation.keys.first] =
+        enrollment.kindergarten_participation.values.first
         end
       end
     end
     @enrollments = @test
   end
 end
+
+binding.pry
+""
