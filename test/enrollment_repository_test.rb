4cf7aff7fa93f027ff7d056cac995e32381bdcfd
alex549us3 <alex549us3@gmail.com>
2017-04-16 13:41:49 -0600

    some tests added for new enrollment repository functions

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -11,6 +11,12 @@ class EnrollmentRepositoryTest < Minitest::Test
                     './data/Kindergartners in full-day program.csv'
                    }
     }
+    @hs_and_kg_data = {
+                        :enrollment => {
+                        :kindergarten => "./data/Kindergartners in full-day program.csv",
+                        :high_school_graduation => "./data/High school graduation rates.csv"
+                        }
+                      }
   end
 
   def test_it_initializes
@@ -34,6 +40,18 @@ class EnrollmentRepositoryTest < Minitest::Test
     assert enrollment.is_a?(Enrollment)
     assert_in_delta 0.144, 
     enrollment.kindergarten_participation_in_year(2004), 0.005
+  end
+
+  def test_loading_both_kg_and_hs_data
+    @er.load_data(@hs_and_kg_data)
+
+    enrollment = @er.find_by_name('ACADEMY 20')
+    assert_equal 'ACADEMY 20', enrollment.name
+    assert_equal 0.302,
+                enrollment.kindergarten_participation_in_year(2004)
+    assert_equal 0.478,
+                enrollment.graduation_rate_in_year(2012)
+
   end
 
 
