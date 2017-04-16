c818f517179bc723e6d192206df1febf3e1cb3b8
alex549us3 <alex549us3@gmail.com>
2017-04-16 17:04:17 -0600

    got rid of more @s

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -35,14 +35,14 @@ class DistrictRepository
   end
 
   def find_by_name(name)
-    @districts.find do |district|
+    districts.find do |district|
       district.name == name
     end
   end
 
   def find_all_matching(name)
     index = name.length - 1
-    @districts.find_all do |district|
+    districts.find_all do |district|
       district.name[0..index] == name[0..index]
     end
   end
