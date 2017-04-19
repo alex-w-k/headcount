f1dd96ff0cfba6337be20cd67a7a0695a73943ae
alex549us3 <alex549us3@gmail.com>
2017-04-19 16:57:34 -0600

    refactoring and fixing lines

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -7,18 +7,10 @@ class EconomicProfile
 
   def initialize(args)
     @name = args[:name]
-    @median_household_income = args[:median_household_income]
-    if @median_household_income.nil?
-      @median_household_income = Hash.new
-    end
-    @children_in_poverty = args[:children_in_poverty]
-    if @children_in_poverty.nil?
-      @children_in_poverty = Hash.new
-    end
-    @free_or_reduced_price_lunch = args[:free_or_reduced_price_lunch]
-    if @free_or_reduced_price_lunch.nil?
-      @free_or_reduced_price_lunch = Hash.new
-    end
+    @median_household_income = args[:median_household_income] || Hash.new
+    @children_in_poverty = args[:children_in_poverty] || Hash.new
+    @free_or_reduced_price_lunch =
+      args[:free_or_reduced_price_lunch] || Hash.new
     @title_i = args[:title_i]
     if @title_i.nil?
       @title_i = Hash.new
